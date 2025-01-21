d148925e88c0bfa231b0abc42317bcb2f44fe784
Beholdthekrivda <lol2002lol2016lol@mail.ru>
2025-01-21 18:36:15 +0700

    new lab

--- Lab1/Lab_1.cpp
+++ Lab1/Lab_1.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <cmath>
 
+using namespace std;
+
 int main()
 {	
 	setlocale(LC_ALL, "RU");
 
+	cout << "”пражнение 1" << endl;
+
 	double func, step, len, x, start, end = 4;
 
-	std::cout << "¬ведите начальное значение (от 0 до 4): ";
-	std::cin >> start;
+	cout << "¬ведите начальное значение (от 0 до 4): ";
+	cin >> start;
 
 	if (0 < start <= 4)
 	{
@@ -16,29 +20,30 @@ int main()
 		step = len / 10;
 		x = start;
 
-		std::cout << "«начение step: " << step << std::endl;
+		cout << "«начение step: " << step << endl;
 
 		while (x <= end)
 		{
 			func = sin(x) / x;
 			x = x + step;
-			std::cout << "«начение функции: " << func << std::endl;
+			cout << "«начение функции: " << func << endl;
 		}
 	}
 
-	std::cout << " " << std::endl;
+	cout << " " << endl;
+	cout << "”пражнение 2" << endl;
 
 	int a = 0, b = 1, c = 0, n = 0, sum = 0;
 	double max;
 
-	std::cout << "¬ведите сумму (max): ";
-	std::cin >> max;
+	cout << "¬ведите сумму (max): ";
+	cin >> max;
 	
 	while (sum < max)
 	{
 		sum = sum + b;
 
-		std::cout << b << " ";
+		cout << b << " ";
 
 		c = a + b;
 		a = b;
@@ -47,9 +52,9 @@ int main()
 		n = n + 1;
 	}
 
-	std::cout << "\n";
-	std::cout << n << std::endl;
-	std::cout << sum << std::endl;
+	cout << "\n";
+	cout << n << endl;
+	cout << sum << endl;
 
 	return 0;
 }
