55b473240c9662d1bae5370d38abd21423ce7955
Beholdthekrivda <lol2002lol2016lol@mail.ru>
2025-01-31 18:57:35 +0700

    new lab, new name

---
+++ Lab4_(10)/Lab4/Lab4.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <time.h>
+#include <stdio.h>
+using namespace std;
+
+void create(int count, char arr[])
+{
+	for (int i = 0; i < count; i++)
+	{
+		arr[i] = rand() % 18;
+	}
+}
+
+void unpacked(int count, char arr[])
+{
+	for (int i = 0; i < count; i++)
+	{
+		printf("%d", arr[i]);
+		printf(" ");
+	}
+}
+// ЗАДАЧА СКОРЕЕ ВСЕГО СДЕЛАНА НЕ ПРАВИЛЬНО
+// но пусть будет как черновик
+void main()
+{
+	setlocale(LC_ALL, "RU");
+
+	srand(time(NULL));
+
+	int n;
+
+	cout << "Введите кол-во эл-ов: ";
+	cin >> n;
+
+	char* nums = new char[n]; int& ptr = n;
+
+	create(ptr, nums);
+	unpacked(ptr, nums);
+}
