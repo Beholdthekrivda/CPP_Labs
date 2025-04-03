40d423c52e3440f49c13b5aa6ba9a3e6791d8863
Beholdthekrivda <lol2002lol2016lol@mail.ru>
2025-04-03 21:50:47 +0700

    new solution for Gauss's method

--- Lab7/Lab7/Lab7.cpp
+++ Lab7/Lab7/Lab7.cpp
@@ -1,65 +1,92 @@
 ﻿#include <iostream>
 #include <fstream>
-
+#include <cmath>
+#include <algorithm>
 using namespace std;
 
 double eps = 0.000001;
 
-void singlesolution(int n, int m, double** matrix, double*& X)
+void singlesolution(int n, int m, double** matrix, double*& x, int* perm)
 {
-    for (int i = min(n - 1, m - 1); i >= 0; i--)
+    double* y = new double[n]; // вводим новый массив для значений неихвестных в марице с переставленными столбцами
+    for (int i = min(n, m) - 1; i >= 0; i--)
     {
-        X[i] = matrix[i][n]; // тк здесь уже должа была получиться матрица ступенчатого вида, то мы берем последний эл-т
-        for (int j = i + 1; j < n; j++) // в этом цикле происходит корректировка
+        y[i] = matrix[i][n];
+        for (int j = i + 1; j < n; j++)
         {
-            X[i] -= matrix[i][j] * X[j]; // а если быть точнее то, j - это предыдщий полученный эл-т
-            // и мы его учитываем для нахождения следующего неизвестного
-            // пример: в матрице 3 * 4 мы нашли x3, а что бы найти x2 мы должны использовать x3, т.к. уравнение будет иметь вид 0*x1 + c1*x2 + c2*x3 = b
-            // или x2 = (b - c2*x3) / c1
-            // в нашем случае c2 = matrix[i][j], x3 = X[j]
+            y[i] -= matrix[i][j] * y[j];
         }
-        X[i] /= matrix[i][i]; // соответсвено c1 = matrix[i][i]
+        y[i] /= matrix[i][i];
     }
+
+    // присваиваем значения массиву x с учетом перестановок
+    for (int i = 0; i < n; i++)
+    {
+        x[perm[i]] = y[i];
+    }
+    delete[] y;
 }
 
-int triangulation(int n, int m, double**& matrix)
+int triangulation(int n, int m, double**& matrix, int* perm)
 {
     int swapcount = 0;
 
     if (m == 0 || n == 0) return swapcount;
 
-    for (int i = 0; i < min(m, n) - 1; i++)
-    {
-        double max = abs(matrix[i][i]); // к в случае с нахождением маскимального эл-та берем за макс самое первое число
-        int maxpos = i; // тоже самое с позицией максимального
 
-        for (int k = i + 1; k < m; k++)
+    for (int i = 0; i < min(m, n) - 1; i++) // метод поиска макс эл-та на всей площади матрицы, кроме последнего столбца
+    {
+        double maxVal = fabs(matrix[i][i]); // как в случае с нахождением маскимального эл-та берем за макс самое первое числ 
+        int maxRow = i, maxCol = i; // также делаем для столбца и строки
+        for (int r = i; r < m; r++)
         {
-            double el = abs(matrix[k][i]); // выбираем из стобца k максимальное значение
-            if (el > max)
+            for (int c = i; c < n; c++)
             {
-                max = el;
-                maxpos = k;
+                double val = fabs(matrix[r][c]);
+                // здесь находим индекс столбца и строки максимального эл-та
+                if (val > maxVal)
+                {
+                    maxVal = val;
+                    maxRow = r;
+                    maxCol = c;
+                }
             }
         }
 
-        if (abs(matrix[maxpos][i]) <= eps) // если максимальный элемент в столбце близок к нулю, то говорим что матрциа вырожденная
+        if (maxVal < eps) // если максимальный элемент близок к нулю, то говорим что матрциа вырожденн
         {
             cout << "Вырожденная матрица" << endl;
             return 0;
         }
 
-        if (i != maxpos) // если ведущий (максимальный) эл-т не в текущей строке, то меняем строки местами
+        if (maxRow != i) // если макс эл-т находится не в i-ой строке, то меняем местами строки i и maxrow
         {
-            double* z = matrix[i]; matrix[i] = matrix[maxpos]; matrix[maxpos] = z;
+            double* temp = matrix[i];
+            matrix[i] = matrix[maxRow];
+            matrix[maxRow] = temp;
             swapcount++;
         }
 
-        for (int j = i + 1; j < m; j++) // обрабатываем строки ниже i-ой строки
+        if (maxCol != i) // если макс эл-т находится не в i-ом столбце, то меняем местами i и maxcol
         {
-            double mul = matrix[j][i] / matrix[i][i]; // определяем во сколько раз эл-т matrix[j][i] больше или меньше ведущего эл-та
-            // это нужно чтобы занулить эл-ты
-            for (int k = i; k < n + 1; k++) // для строки j (которая ниже строки i) обнуляем эл-ты 
+            for (int r = 0; r < m; r++)
+            {
+                double temp = matrix[r][i];
+                matrix[r][i] = matrix[r][maxCol];
+                matrix[r][maxCol] = temp;
+            }
+            // замена строк происходит без последствий, но замена строк ведет к изменению порядка записи переменных в системе
+            // поэтому чтобы в дальнейшем сохранить порядок вводим массив, в котором будем отслеживать перестановки столбцов 
+            int tempPerm = perm[i];
+            perm[i] = perm[maxCol];
+            perm[maxCol] = tempPerm;
+            swapcount++;
+        }
+
+        for (int j = i + 1; j < m; j++)
+        {
+            double mul = matrix[j][i] / matrix[i][i];
+            for (int k = i; k < n + 1; k++)
             {
                 matrix[j][k] -= matrix[i][k] * mul;
             }
@@ -67,7 +94,7 @@ int triangulation(int n, int m, double**& matrix)
     }
 
     int last = min(m, n) - 1;
-    if (abs(matrix[last][last]) < eps)
+    if (fabs(matrix[last][last]) <= eps)
     {
         cout << "Вырожденная матрица" << endl;
         return 0;
@@ -76,52 +103,56 @@ int triangulation(int n, int m, double**& matrix)
     return swapcount;
 }
 
-void main()
+int main()
 {
-	setlocale(LC_ALL, "RU");
+    setlocale(LC_ALL, "RU");
 
-	ifstream file("test.txt");
+    ifstream file("test.txt");
 
-	int m, n;
-
-	while (file >> m >> n)
-	{
+    int m, n;
+    while (file >> m >> n)
+    {
         double** matrix = new double* [m];
-        for (int i = 0; i < m; i++) 
+        for (int i = 0; i < m; i++)
         {
             matrix[i] = new double[n + 1];
         }
 
-        for (int i = 0; i < m; i++) 
+        for (int i = 0; i < m; i++)
         {
-            for (int j = 0; j < n + 1; j++) 
+            for (int j = 0; j < n + 1; j++)
             {
                 file >> matrix[i][j];
             }
         }
 
-		for (int i = 0; i < m; i++)
-		{
-			for (int j = 0; j < n + 1; j++)
-			{
-				cout << matrix[i][j] << " ";
-			}
-			cout << endl;
-		}
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n + 1; j++)
+            {
+                cout << matrix[i][j] << " ";
+            }
+            cout << endl;
+        }
         cout << endl;
 
         double* x = new double[n];
-
         for (int i = 0; i < n; i++)
         {
             x[i] = 0;
         }
 
-        int result = triangulation(n, m, matrix);
+        int* perm = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            perm[i] = i;
+        }
+
+        int result = triangulation(n, m, matrix, perm);
 
         if (result)
         {
-            singlesolution(n, m, matrix, x);
+            singlesolution(n, m, matrix, x, perm);
 
             for (int i = 0; i < m; i++)
             {
@@ -131,21 +162,22 @@ void main()
                 }
                 cout << endl;
             }
-            
+
             for (int i = 0; i < n; i++)
             {
-                cout << x[i] << " ";
+                cout << "x" << i + 1 << "=" << x[i] << " ";
             }
             cout << endl;
         }
         cout << endl;
 
-        for (int i = 0; i < m; i++) 
-        {
+        for (int i = 0; i < m; i++)
             delete[] matrix[i];
-        }
         delete[] matrix;
-	}
+        delete[] x;
+        delete[] perm;
+    }
 
     file.close();
+    return 0;
 }
