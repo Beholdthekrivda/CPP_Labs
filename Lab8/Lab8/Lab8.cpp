2af5607b2334c6a68fd89ac1f4ba6d6d9fdeb444
Beholdthekrivda <lol2002lol2016lol@mail.ru>
2025-04-19 21:14:15 +0700

    add lesson3 and solution

--- Lab8/Lab8/Lab8.cpp
+++ Lab8/Lab8/Lab8.cpp
@@ -1,15 +1,15 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <string>
-#include <filesystem>
+
 using namespace std;
 
-void matrix(int n, int m, const int* v1, const int* v2, const string& matrix_file) 
+void matrix(int n, int m, int* v1, int* v2, const string& matrix_file) 
 {
     ofstream out(matrix_file);
-    out << n << endl;
+    out << n << " " << m << endl;
 
-    // Строим временную матрицу
+    // Строим матрицу смежноси
     int** M = new int* [n];
     for (int i = 0; i < n; ++i) 
     {
@@ -43,8 +43,8 @@ void matrix(int n, int m, const int* v1, const int* v2, const string& matrix_fil
 void graph(const string& matrix_file, const string& graph_file, int*& L) 
 {
     ifstream in(matrix_file);
-    int n;
-    in >> n;  // читаем размер
+    int n, m;
+    in >> n >> m;  // читаем размер
 
     // Загружаем матрицу
     int** M = new int* [n];
@@ -58,22 +58,12 @@ void graph(const string& matrix_file, const string& graph_file, int*& L)
     }
     in.close();
 
-    // Считаем количество рёбер m (неориентированный граф)
-    int m = 0;
-    for (int i = 0; i < n; ++i)
-    {
-        for (int j = i + 1; j < n; ++j)
-        {
-            if (M[i][j]) ++m;
-        }
-    }
-    
     // Степени вершин L[i]
     for (int i = 0; i < n; ++i)
     {
         for (int j = 0; j < n; ++j)
         {
-            if (M[i][j]) L[i]++;
+            if (M[i][j]) L[i]++; // считаем кол-во смежных вершин для i-ой вершины
         }
     }
 
@@ -94,7 +84,7 @@ void graph(const string& matrix_file, const string& graph_file, int*& L)
         }
     }
 
-    // Записываем упорядоченный список в graph_k.txt
+    // Записываем в graph_k.txt
     ofstream out(graph_file);
     out << n << " " << m << endl;
     for (int i = 0; i < n; ++i) 
@@ -143,25 +133,23 @@ void paths(const string& graph_file, const string& paths_file, int* L)
     in.close(); out.close();
 }
 
-int main() 
+void lesson1()
 {
-    setlocale(LC_ALL, "RU");
-
     ifstream file("Graph.txt");
 
     int n, m, graph_num = 1;
-    while (file >> n >> m) 
+    while (file >> n >> m)
     {
         // читаем рёбра
-        int* v1 = new int[m];
-        int* v2 = new int[m];
-        for (int k = 0; k < m; ++k) 
+        int* v1 = new int[m]; // массив левых вершин
+        int* v2 = new int[m]; // массив правых вершин
+        for (int k = 0; k < m; ++k)
         {
             file >> v1[k] >> v2[k];
             --v1[k]; --v2[k];
         }
 
-        int* L = new int[n]();
+        int* L = new int[n](); // массив кол-ва смежных вершин для каждой i-ой вершины
 
         // имена файлов
         string matrix_file = "C:/Users/Bruh263/Desktop/CPP_Labs/Lab8/Lab8/matrices/matrix_" + to_string(graph_num) + ".txt";
@@ -177,5 +165,83 @@ int main()
     }
 
     file.close();
+}
+
+void hamilton(int k, int n, int** M, int* P, int* R, int& count)
+{
+    int i = P[k - 1];
+
+    if (i == n - 1)
+    {
+        for (int i = 0; i < k; i++)
+            cout << P[i] + 1 << (i + 1 < k ? " -> " : "");
+        count++;
+        cout << endl;
+        return;
+    }
+
+    for (int j = 0; j < n; j++)
+    {
+        if ((R[j] == 0) && M[i][j])
+        {
+            P[k] = j;
+            R[j] = 1;
+            hamilton(k + 1, n, M, P, R, count);
+            R[j] = 0;
+        }
+    }
+}
+
+void lesson3()
+{
+    ifstream file("directed_graph.txt");
+
+    int n, m;
+    while (file >> n >> m)
+    {
+        int** M = new int* [n];
+        for (int i = 0; i < n; i++) M[i] = new int[n]();
+
+        int v, u;
+        for (int i = 0; i < m; i++)
+        {
+            file >> v >> u;
+            M[v - 1][u - 1] = 1;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cout << M[i][j] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+
+        int* R = new int[n]();
+        int* P = new int[n]();
+        P[0] = 0;
+        R[0] = 1;
+        int count = 0;
+        hamilton(1, n, M, P, R, count);
+
+        cout << "Количество путей: " << count << endl;
+
+        for (int i = 0; i < n; i++) delete M[i];
+        delete[] M;
+        delete[] P;
+        delete[] R;
+    }
+
+}
+
+int main() 
+{
+    setlocale(LC_ALL, "RU");
+
+    cout << "Упражнение 3" << endl;
+    lesson3();
+
     return 0;
 }
